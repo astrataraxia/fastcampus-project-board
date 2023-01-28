21072e4d84c3522b6961777f282e49e030297c76
astrataraxia <astrataraxia@gmail.com>
2023-01-28 16:36:27 +0900

    #17 - UserAccount Entity생성.
    Article , ArtcicleComment 정리 및 UserAccount 엔티티 생성 UserAccountRepository 생성. 테스트도 추가.

--- src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
@@ -1,6 +1,5 @@
 package com.fastcampus.projectboard.controller;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +10,10 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-@Disabled("Spring Data Rest Test는 통합 테스트에 제외")
+//@Disabled("Spring Data Rest Test는 통합 테스트에 제외")
 @DisplayName("Data Rest Test")
 @AutoConfigureMockMvc
 @Transactional
@@ -79,4 +78,16 @@ public class DataRestTest {
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
     }
 
+    @Test
+    @DisplayName("[api] 회원 관련 API는 일체 제공하지 않는다.")
+    void givenNothing_whenRequestingUserAccounts_thenThrowsException() throws Exception {
+        // Given
+        // When & Then
+        mvc.perform(get("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(post("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(put("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(patch("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(delete("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(head("/api/userAccounts")).andExpect(status().isNotFound());
+    }
 }
