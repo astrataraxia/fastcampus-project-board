f62834c4dd9ccd9d4dbfe5996923d116895c4722
astrataraxia <astrataraxia@gmail.com>
2023-02-11 11:22:26 +0900

    #62 - swagger를 이용한 API  doc

--- src/main/java/com/fastcampus/projectboard/controller/MainController.java
+++ src/main/java/com/fastcampus/projectboard/controller/MainController.java
@@ -1,13 +1,43 @@
 package com.fastcampus.projectboard.controller;
 
+import com.fastcampus.projectboard.dto.response.ArticleCommentResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 메인 컨트롤러
+ *
+ * <p>
+ *     테스트 중입니다.
+ * </p>
+ */
 @Controller
 public class MainController {
 
     @GetMapping("/")
     public String root() {
         return "forward:/articles";
+    }
+
+    /**
+     * 댓글 정보를 열람한다
+     * @param id 댓글 ID
+     * @return 댓글 응답
+     */
+    @ResponseBody
+    @GetMapping("/test-rest")
+    public ArticleCommentResponse test(Long id) {
+        return ArticleCommentResponse.of(
+                id,
+                "content",
+                LocalDateTime.now(),
+                "e@mail.com",
+                "Uno",
+                "uno"
+        );
     }
 }
